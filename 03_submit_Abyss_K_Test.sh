298d714a7be96ec11592f521027fd6c7cd20a7f1
Amanda Charbonneau <charbo24@msu.edu>
2016-08-09 10:08:10 -0400

    new spades scripts

--- 03_submit_Abyss_K_Test.sh
+++ 03_submit_Abyss_K_Test.sh
@@ -1,3 +1,2 @@
 mkdir k{51..63}
-qsub -N ecoli -pe openmpi 8 -t 51-63:2 \
-    <<<'abyss-pe -C k$SGE_TASK_ID in='Cr_P_1.fastq  Cr_P_2.fastq' se='Cr_U_1.fastq  Cr_U_2.fastq'
+qsub -N ecoli -pe openmpi 8 -t 51-63:2 <<<'abyss-pe -C k$SGE_TASK_ID in='Cr_P_1.fastq  Cr_P_2.fastq' se='Cr_U_1.fastq  Cr_U_2.fastq'
---
+++ 04_submit_SPADES_K_Test.sh
@@ -0,0 +1,1 @@
+qsub ../effective-octo-fortnight/4_SPADES_Lichen.qsub -N SPADES_lichen_51  -v kmer=52
