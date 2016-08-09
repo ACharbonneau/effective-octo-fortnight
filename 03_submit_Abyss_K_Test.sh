174fcf44a7ffb0f8ece3efbaf0e498ddbcbba644
Amanda Charbonneau <charbo24@msu.edu>
2016-08-09 13:51:21 -0400

    revised qsub

--- 03_submit_Abyss_K_Test.sh
+++ 03_submit_Abyss_K_Test.sh
@@ -1,2 +1,1 @@
-mkdir k{51..63}
-qsub -N ecoli -pe openmpi 8 -t 51-63:2 <<<'abyss-pe -C k$SGE_TASK_ID in='Cr_P_1.fastq  Cr_P_2.fastq' se='Cr_U_1.fastq  Cr_U_2.fastq'
+qsub ../../effective-octo-fortnight/3_abyss_Lichen.qsub -N abyss_lichen_47  -v kmer=47
--- 04_submit_SPADES_K_Test.sh
+++ 04_submit_SPADES_K_Test.sh
@@ -1,1 +1,1 @@
-qsub ../effective-octo-fortnight/4_SPADES_Lichen.qsub -N SPADES_lichen_51  -v kmer=52
+qsub ../../effective-octo-fortnight/4_SPADES_Lichen.qsub -N SPADES_lichen_47  -v kmer=47
