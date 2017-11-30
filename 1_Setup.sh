f9297d87bb2476f5fc5f2486d8d847197981b9e0
Amanda Charbonneau <charbo24@msu.edu>
2017-11-30 16:45:24 -0500

    dump permissions on seq copy

--- 1_Setup.sh
+++ 1_Setup.sh
@@ -4,20 +4,20 @@ module load SRAToolkit/2.8.2
 
 ## Set up directory structure
 
-mkdir RawReads Metadata ProcessedReads PublicData QualityControl QualityControl/raw QualityControl/trimmed
+mkdir RawReads Metadata ProcessedReads PublicData PublicData/Junttila PublicData/PhiX QualityControl QualityControl/raw QualityControl/trimmed
 
 
 ## Get data
 cd RawReads
-cp ~/Crangiferina/CrX1_GCCAAT_L001_R1_001.fastq.gz .
-cp ~/Crangiferina/CrX1_GCCAAT_L001_R2_001.fastq.gz .
-
+cp  ~/Crangiferina/CrX1_GCCAAT_L001_R1_001.fastq.gz .
+cp  ~/Crangiferina/CrX1_GCCAAT_L001_R2_001.fastq.gz .
+chmod 777 *
 
 cd ../Metadata
 cp /mnt/research/radishGenomics/PublicData/alladaptors.fa .
 cp ~/Metadata/Crangiferina/20160714_SeqProduction_Dworkin.csv .
 
-cd ../PublicData
+cd ../PublicData/Junttila
 ## Paper 10.1186/1471-2164-13-575
 # assembled transcriptome contigs
 wget https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3534622/bin/1471-2164-13-575-S1.zip
@@ -26,6 +26,7 @@ unzip 1471-2164-13-575-S1.zip
 fastq-dump SRR446707
 # raw Sanger reads GenBank:JK811361-JK813924 GenBank:GH717691-GH717859
 cp ~/Crangiferina/Public/JunttilaSanger.fa .
+cd ../PhiX
 cp ~/Crangiferina/Public/PhiX.fasta .
 
 cd ..
