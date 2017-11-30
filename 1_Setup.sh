6489029a003463fd1ef6cde3e1b66d049f94af8e
Amanda Charbonneau <charbo24@msu.edu>
2017-11-30 16:36:46 -0500

    remove phiX upfront

--- 1_Setup.sh
+++ 1_Setup.sh
@@ -4,7 +4,7 @@ module load SRAToolkit/2.8.2
 
 ## Set up directory structure
 
-mkdir RawReads Metadata ProcessedReads PublicData QualityControl
+mkdir RawReads Metadata ProcessedReads PublicData QualityControl QualityControl/raw QualityControl/trimmed
 
 
 ## Get data
@@ -21,7 +21,12 @@ cd ../PublicData
 ## Paper 10.1186/1471-2164-13-575
 # assembled transcriptome contigs
 wget https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3534622/bin/1471-2164-13-575-S1.zip
+unzip 1471-2164-13-575-S1.zip
 # raw short reads
 fastq-dump SRR446707
 # raw Sanger reads GenBank:JK811361-JK813924 GenBank:GH717691-GH717859
 cp ~/Crangiferina/Public/JunttilaSanger.fa .
+cp ~/Crangiferina/Public/PhiX.fasta .
+
+cd ..
+qsub effective-octo-fortnight/1.1_FastQC_Lichen.qsub
