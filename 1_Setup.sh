60b1fa8ec3f9dece046a0eaa302b4e9324adc3e4
Amanda Charbonneau <charbo24@msu.edu>
2017-11-30 14:52:49 -0500

    begining to fix repo

---
+++ 1_Setup.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+module load SRAToolkit/2.8.2
+
+## Set up directory structure
+
+mkdir RawReads Metadata ProcessedReads PublicData QualityControl
+
+
+## Get data
+cd RawReads
+cp ~/Crangiferina/CrX1_GCCAAT_L001_R1_001.fastq.gz .
+cp ~/Crangiferina/CrX1_GCCAAT_L001_R2_001.fastq.gz .
+
+
+cd ../Metadata
+cp /mnt/research/radishGenomics/PublicData/alladaptors.fa .
+cp ~/Metadata/Crangiferina/20160714_SeqProduction_Dworkin.csv .
+
+cd ../PublicData
+## Paper 10.1186/1471-2164-13-575
+# assembled transcriptome contigs
+wget https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3534622/bin/1471-2164-13-575-S1.zip
+# raw short reads
+fastq-dump SRR446707
+# raw Sanger reads GenBank:JK811361-JK813924 GenBank:GH717691-GH717859
+cp ~/Crangiferina/Public/JunttilaSanger.fa .
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # effective-octo-fortnight
 
-In progress scripts for building the Cladonia rangiferina genome
+Assembly scripts for Cladonia rangiferina Genome Project Edit
+
+tldr:
+
+ 1. Make a directory and cd into it
+  > mkdir crang && cd crang
+
+ 2. git clone this repo
+  > git clone https://github.com/ACharbonneau/effective-octo-fortnight.git
+
+ 3. run setup script from main directory
+  > bash effective-octo-fortnight/1_Setup.sh
+  
